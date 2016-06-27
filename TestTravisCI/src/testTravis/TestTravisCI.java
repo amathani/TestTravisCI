34deb3a422f0f8e449aa0c32a9dab8de1ad0d146
Amit Athani <donathani@gmail.com>
2016-06-28 00:00:02 +0530

    Added travis and ant build

---
+++ TestTravisCI/src/testTravis/TestTravisCI.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package testTravis;
+
+/**
+ * @author Amit Athani
+ *
+ */
+public class TestTravisCI {
+	
+	public static boolean testIsPrime(int prime) {
+		return false;
+	}
+
+}
