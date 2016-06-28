ce6b507e6d31728867254a04de36b0b8dc6e5fbf
Amit Athani <donathani@gmail.com>
2016-06-28 18:41:45 +0530

    Added updated ant buildfile

---
+++ src/testTravis/UnitTests.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package testTravis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Amit Athani
+ *
+ */
+public class UnitTests {
+
+	/**
+	 * Test method for {@link testTravis.TestTravisCI#testIsPrime(int)}.
+	 */
+	@Test
+	public void testTestIsPrime() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
